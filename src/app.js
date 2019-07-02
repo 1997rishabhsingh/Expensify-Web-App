c64dc885026228946f6bedb4055ac21b8bd7a061
Rishabh Singh <1997rishabhsingh@gmail.com>
2019-07-02 21:00:43 +0530

    Update scripts

--- src/app.js
+++ src/app.js
@@ -5,12 +5,10 @@ import configureStore from './store/configureStore'
 import { Provider } from 'react-redux'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-
 import 'react-dates/lib/css/_datepicker.css'
 import 'react-dates/initialize';
 
 const store = configureStore()
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
